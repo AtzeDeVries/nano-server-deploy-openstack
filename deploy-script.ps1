a8716ee8c463917db38162bef365c071be2885e5
Atze de Vries <AtzeDeVries@users.noreply.github.com>
2018-07-05 10:07:31 +0200

    Update deploy-script.ps1

--- deploy-script.ps1
+++ deploy-script.ps1
@@ -1,5 +1,14 @@
 #ps1
 
+# what until disk is attached.
+
+# wait unil disk with serialnumber is not equal 0
+while ( (Get-Disk | where-object Serialnumber -ne "" | measure).count -eq 0 ) {
+  Write-Host "Waiting 60 seconds to try again"
+  Start-Sleep -s 60
+}
+
+
 # Find new openstack disk and initalize
 get-disk | Where-Object SerialNumber -ne "" | Initialize-Disk
 
@@ -11,12 +20,4 @@ get-disk | Where-Object SerialNumber -ne "" | set-disk -isreadonly 0
 
 # format with ntfs
 get-disk | Where-Object SerialNumber -ne "" | get-partition | Where-Object Type -ne "Reserved" | Format-Volume -FileSystem NTFS -Confirm
-
-# setup credentials to connect to ad
-$password = ConvertTo-SecureString -asPlainText -Force ""
-$username = ""
-$ComputerName = ""
-
-# join to the domain has to be done from the DC
-
 
